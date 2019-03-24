2af69caa65ca4868143c474a038b4cf0d14133d7
JoshuaHolloway <jhollow6@asu.edu>
2019-03-24 11:56:31 -0700

    cmake adding a library

---
+++ 2_cmake/01_adding_library/build/CMakeFiles/josh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/josh.dir/main.cpp.o"
+  "josh.pdb"
+  "josh"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/josh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
